9cdab664a28b3b95ecf77065218c642ab72aacde
Sam <sam.sutcliffe@durham.ac.uk>
2025-12-15 19:19:34 +0000

    Update ice.lisp

--- ice.lisp
+++ ice.lisp
@@ -74,6 +74,6 @@
                      :plotter (lambda (sim) (plot-domain))
                      :post-conv-step (lambda (sim) (plot-domain)))))
           (format t "Stability:~E ~E ~A   ~%" height flotation res)
-          (save-stabilty-data stability-dir *sim* res height flotation)
+          (save-stabilty-data stability-dir *sim* res height flotation *notch-ratio*)
           (cl-mpm/dynamic-relaxation::save-vtks *sim* output-dir 1)
           )))))
